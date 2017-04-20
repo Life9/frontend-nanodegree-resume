6c743fde6a6ad6a7fbaf7dfcf48a9a5fd895eb0a
= <=>
2017-04-20 04:45:42 -0400

    Fixed Code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,48 @@
+//Konami Code
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length)
+      activateCheats();
+  } else
+    konamiCodePosition = 0;
+});
+
+function activateCheats() {
+    document.body.style.backgroundColor = "black";
+    
+    var audio = new Audio("music/code.mp3");
+    audio.play();
+
+    alert("Konami Code Activated!!!!");
+}
+
 //Objects with loops
 var education = {
     "schools": [{
@@ -191,48 +236,6 @@ var projects = {
 // Google Map
 $("#mapDiv").append(googleMap);
 
-//Konami Code
-function konami() {
-    var allowedKeys = {
-        37: 37,
-        38 : 38,
-        39 : 39,
-        40 : 40,
-        65 : 65,
-        66 : 66
-    }
-        // Code is Up Up Down Down Left Right Left Right B A
-    var konamiCode = ["38","38","40","40","37","39","37","39","66","65"]
-    var codePosition = 0
-    document.addEventListener('keydown', function(k) {
-        var key = allowedKeys[k.keyCode];
-        var requiredKey = konamiCode[codePosition];
-
-        if (key === requiredKey) {
-            codePosition++;
-
-            if (codePosition == konamiCode.length) {
-                activateCheats();
-            }
-
-            else {
-            codePosition = 0;
-            }
-        }
-    });
-}
-
-function activateCheats() {
-    document.body.style.backgroundImage = "url("images/code.jpg")";
-    var audio = new Audio("music/code.mp3");
-    audio.play();
-    
-    alert("Konami Code Activated!!!!");
-}
-
-
-
-}
 
 // Page Click event, possible heat map functionality
 // $(document).click(function(loc) {
@@ -253,6 +256,7 @@ function activateCheats() {
 // $("#main").append(internationalizeButton);
 
 // Calls all the functions
+
 education.display();
 work.display();
 projects.display();
