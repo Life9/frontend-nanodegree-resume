e7dbbdee1bf291519c225785bf3839193c3a1a5e
= <=>
2017-04-20 04:50:39 -0400

    Code Cleanup

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,26 +8,17 @@ var allowedKeys = {
   66: 'b'
 };
 
-// the 'official' Konami Code sequence
 var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-
-// a variable to remember the 'position' the user has reached so far.
 var konamiCodePosition = 0;
 
-// add keydown event listener
 document.addEventListener('keydown', function(e) {
-  // get the value of the key code from the key map
+
   var key = allowedKeys[e.keyCode];
-  // get the value of the required key from the konami code
   var requiredKey = konamiCode[konamiCodePosition];
 
-  // compare the key with the required key
   if (key == requiredKey) {
-
-    // move to the next key in the konami code sequence
     konamiCodePosition++;
 
-    // if the last key is reached, activate cheats
     if (konamiCodePosition == konamiCode.length)
       activateCheats();
   } else
@@ -36,7 +27,7 @@ document.addEventListener('keydown', function(e) {
 
 function activateCheats() {
     document.body.style.backgroundColor = "black";
-    
+
     var audio = new Audio("music/code.mp3");
     audio.play();
 
